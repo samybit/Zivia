d9bb0c1b883e745baef90a12ecb08870fe1dd1fd
samysamir-dev <samyb.samir@gmail.com>
2025-08-08 23:06:25 +0300

    make the colors Dame Dark and fix bugs

--- ui.py
+++ ui.py
@@ -1,7 +1,11 @@
 from tkinter import *
+from question_model import Question
 from quiz_brain import QuizBrain
 
-THEME_COLOR = "#000000"
+THEME_COLOR = "#B6BAB3"
+QUESTION_COLOR = "#121015"
+SCORE_COLOR = "#D71134"
+CANVAS_COLOR = "#A8B082"
 
 class QuizInterface:
     def __init__(self, quiz_brain: QuizBrain):
@@ -13,18 +17,28 @@ class QuizInterface:
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
 
         # Score label
-        self.score_label = Label(text="Score: 0", fg="#FFD54F", bg=THEME_COLOR, font=("Luckiest Guy", 20, "bold"))
+        self.score_label = Label(text="Score: 0", 
+            fg=SCORE_COLOR, 
+            bg=THEME_COLOR, 
+            font=("Luckiest Guy", 20, "bold"), 
+            )
         self.score_label.grid(row=0, column=1)
 
         # Canvas to display the question
-        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.canvas = Canvas(
+            width=350, 
+            height=250, 
+            bg="green", 
+            highlightthickness=4, 
+            highlightbackground=SCORE_COLOR,
+            )
         self.question_text = self.canvas.create_text(
             150,
             125,
-            width=280,
+            width=310,
             text="Question goes here",
-            fill=THEME_COLOR,
-            font=("Arial", 20, "italic"),
+            fill=QUESTION_COLOR,
+            font=("Luckiest Guy", 20, "bold"),
         )
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
@@ -37,8 +51,8 @@ class QuizInterface:
             relief="flat",
             bg=THEME_COLOR,
             activebackground=THEME_COLOR,
-            command=self.true_pressed)
-        self.true_button.image = true_image  # prevents python garbage collection
+            command=self.true_pressed,
+            )
         self.true_button.grid(row=2, column=0)
 
         false_image = PhotoImage(file="images/false.png")
@@ -49,8 +63,8 @@ class QuizInterface:
             relief="flat",
             bg=THEME_COLOR,
             activebackground=THEME_COLOR,
-            command=self.false_pressed)
-        self.false_button.image = false_image  # prevents python garbage collection
+            command=self.false_pressed,
+            )
         self.false_button.grid(row=2, column=1)
 
         self.get_next_question()
@@ -58,10 +72,10 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        self.canvas.config(bg="#FFFFFF")
+        self.canvas.config(bg=CANVAS_COLOR)
 
         if self.quiz.still_has_questions():
-            self.score_label.config(text=f"Score: {self.quiz.score}|{self.quiz.question_number}")
+            self.score_label.config(text=f"Score: {self.quiz.score}|10", font=("Luckiest Guy", 20, "bold"))
             q_text = self.quiz.next_question()
             self.canvas.itemconfig(self.question_text, text=q_text)
         else:
