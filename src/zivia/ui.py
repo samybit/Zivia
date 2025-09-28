90dee66cb7247e36bc0fe475e8186a35b9a3b7a6
samysamir-dev <samyb.samir@gmail.com>
2025-09-28 08:10:37 +0300

    add wanted files for publishing and wrap main code in function

---
+++ src/zivia/ui.py
@@ -0,0 +1,111 @@
+import os, sys
+from tkinter import * # type: ignore
+from .question_model import Question
+from .quiz_brain import QuizBrain
+
+THEME_COLOR = "#B6BAB3"
+QUESTION_COLOR = "#121015"
+SCORE_COLOR = "#D71134"
+CANVAS_COLOR = "#A8B082"
+
+
+def resource_path(relative_path):
+    """ Get the absolute path to a resource, works for dev and for PyInstaller """
+    try:
+        # When running as .exe
+        base_path = sys._MEIPASS # type: ignore
+    except AttributeError:
+        # When running as .py
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+class QuizInterface:
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
+        # Window setup
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(bg=THEME_COLOR, padx=20, pady=20)
+
+        # Score label
+        self.score_label = Label(text="Score: 0", 
+            fg=SCORE_COLOR, 
+            bg=THEME_COLOR, 
+            font=("Luckiest Guy", 20, "bold"), 
+            )
+        self.score_label.grid(row=0, column=1)
+
+        # Canvas to display the question
+        self.canvas = Canvas(
+            width=350, 
+            height=250, 
+            bg="green", 
+            highlightthickness=4, 
+            highlightbackground=SCORE_COLOR,
+            )
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width=310,
+            text="Question goes here",
+            fill=QUESTION_COLOR,
+            font=("Luckiest Guy", 20, "bold"),
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        # Buttons for true and false
+        true_image = PhotoImage(file=resource_path("zivia/images/true.png"))
+        self.true_button = Button(
+            image=true_image, 
+            highlightthickness=0, 
+            bd=0, 
+            relief="flat",
+            bg=THEME_COLOR,
+            activebackground=THEME_COLOR,
+            command=self.true_pressed,
+            )
+        self.true_button.grid(row=2, column=0)
+
+        false_image = PhotoImage(file=resource_path("zivia/images/false.png"))
+        self.false_button = Button(
+            image=false_image, 
+            highlightthickness=0, 
+            bd=0, 
+            relief="flat",
+            bg=THEME_COLOR,
+            activebackground=THEME_COLOR,
+            command=self.false_pressed,
+            )
+        self.false_button.grid(row=2, column=1)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg=CANVAS_COLOR)
+
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}|10", font=("Luckiest Guy", 20, "bold"))
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="Quiz Ended.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_pressed(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+        
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="#00FF00")
+        else:
+            self.canvas.config(bg="#FF0000")
+        self.window.after(1000, self.get_next_question)
---
+++ zivia/quiz_brain.py
@@ -0,0 +1,56 @@
+import html
+
+class QuizBrain:
+
+    def __init__(self, q_list):
+        """
+        Constructor for QuizBrain object.
+
+        Args:
+            q_list (list): A list of Question objects.
+
+        Sets the question number to 0, the score to 0, the question list to the input list,
+        and the current question to None.
+        """
+        self.question_number = 0
+        self.score = 0
+        self.question_list = q_list
+        self.current_question = None
+
+    def still_has_questions(self):
+        """
+        Checks if there are any questions left.
+
+        Returns:
+            bool: True if there are still questions left, False otherwise.
+        """
+        return self.question_number < len(self.question_list)
+
+    def next_question(self):
+        self.current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        q_text = html.unescape(self.current_question.text)
+
+        return f"Q.{self.question_number}: {q_text}"
+
+    def check_answer(self, user_answer):
+        """
+        Checks if the user's answer is correct.
+        
+        Args:
+            user_answer (str): The user's answer to the current question.
+        
+        Returns:
+            None
+        """
+        # check to make sure self.current_question is not None before accessing its answer attribute.
+        if self.current_question is None:   
+            print("No current question set.")
+            return 
+        
+        correct_answer = self.current_question.answer
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            return True
+        else:
+            return False
